143f7c751ea3dc9e3c98edbfbf79b4101143122f
Rahul151995 <rahul151995@gmail.com>
2018-10-06 23:52:19 +0530

    donor form

--- src/app/home/bulb-activity/bulb-activity.component.ts
+++ src/app/home/bulb-activity/bulb-activity.component.ts
@@ -9,6 +9,7 @@ import {
 } from "@angular-redux/store";
 import { IAppState } from "../../store";
 import { DataServiceService } from "../msgService/data-service.service";
+declare var $: any;
 @Component({
   selector: "app-bulb-activity",
   templateUrl: "./bulb-activity.component.html",
@@ -16,12 +17,22 @@ import { DataServiceService } from "../msgService/data-service.service";
 })
 export class BulbActivityComponent implements OnInit {
   constructor() { }
-
+bloodAplus:any=3;
+bloodBplus:any=8;
+bloodOplus:any= 2;
+bloodABplus:any = 5;
+bloodAminus:any = 9;
+bloodBminus:any = 6;
+bloodOminus:any =8;
+bloodABminus:any=3;
   ngOnInit() {
 
   }
 
-
+  bloodDonateForm()
+  {
+    $("#bloodDonorForm").modal('show');
+  }
 
 
 }
